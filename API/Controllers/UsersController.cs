281fbcb76f89bc833c81d9a3a4850257edd72496
ParamanandR <paramanand.rajbhar.git@gmail.com>
2024-11-24 15:02:35 +0530

    Added ExtensionMethod

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,12 +1,15 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
+[Authorize]
 public class UsersController(DataContext context) : BaseApiController
 {
+    
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
