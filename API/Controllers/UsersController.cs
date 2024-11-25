0dade3d6e34a553a3d69cb640cd6e316bea5649e
ParamanandR <paramanand.rajbhar.git@gmail.com>
2024-11-25 22:07:20 +0530

    Frontend- login/logout

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,7 +9,8 @@ namespace API.Controllers;
 [Authorize]
 public class UsersController(DataContext context) : BaseApiController
 {
-    
+
+    [AllowAnonymous] 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
